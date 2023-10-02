3bfdddd7908696c63421196a44cd050a3359c4a2
evie-adenoyin <ekong.emmanuel@lmu.edu.ng>
2023-10-03 00:46:58 +0100

    Path settings research methodology

--- testing.py
+++ testing.py
@@ -4,37 +4,23 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 
 
-service = Service(executable_path=r'C:\Users\Django\Downloads\chromedriver-win64\chromedriver.exe')
-driver = webdriver.Chrome(service = service)
-driver.implicitly_wait(4)
+website = "https://fp.lmu.edu.ng"
+path =r"\Users\Django\Downloads\chromedriver-win64\chromedriver.exe"
+service = Service(executable_path=path)
+driver = webdriver.Chrome(service=service)
+
 
 # get school login page 
-driver.get("https://fp.lmu.edu.ng")
+driver.get(website)
 
 
 # form interactions 
-username = driver.find_element(By.NAME, 'username')
-password = driver.find_element(By.NAME, 'password')
-login_btn = driver.find_element(By.CLASS_NAME, 'button')
+username = driver.find_element(by='xpath', value="//input[@id='username']")
+password = driver.find_element(by='name', value= 'password')
+login_btn = driver.find_element(by='xpath', value="//input[@type='submit']")
 
 
 # User credentials 
 username.send_keys('ekong.emmanuel')
-password.send_keys('password')
+password.send_keys('2020lmucom2020')
 login_btn.click()
-
-# Receipt print out 
-trans_payment_receipt= driver.find_elements(By.CLASS_NAME,'trans_payment_receipt')[0]
-trans_payment_receipt.click()
-
-# profile interactions 
-menulist_ul = driver.find_element(By.ID, 'menulist_ul')
-dropdown_list = menulist_ul.find_elements(By.TAG_NAME, 'li')[2]
-print(dropdown_list.text)
-item_list = dropdown_list.find_element(By.TAG_NAME,'a')
-
-
-# dropdown_list = driver.find_elements(By.CLASS_NAME, 'dropdown-toggle')[2]
-# dropdown_list.click()
-# dropdown_menu = driver.find_elements(By.CLASS_NAME,'dropdown-menu')[1]
-# dropdown_menu.click()
